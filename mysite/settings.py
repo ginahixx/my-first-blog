ceaee3162532f92721dc8448f2f0bf4a4ec881c0
gina <ginahixx@gmail.com>
2016-11-05 15:50:36 -0700

    added view and template for post detail and css fun stuff

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '6exp9@$^&gok83(7vzbs^9@mm^8)23en^!%nc7bji#4t0u8=i%'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -120,4 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT =  os.path.join(os.path.join(BASE_DIR, 'blog'), "static")
+
+print "Static root is:"
+print STATIC_ROOT
